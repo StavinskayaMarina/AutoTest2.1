de1fce3745e38f8cfbbe82ddeda499d04e2210b8
Stavinskaya_M <marina.st6277@gmail.com>
2023-07-18 15:48:52 +0600

    WebTestSettings

---
+++ src/test/java/ru/netology/CardWebTest.java
@@ -0,0 +1,29 @@
+package ru.netology;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class CardWebTest {
+    private WebDriver driver;
+
+
+    @BeforeAll
+    public static void setupAll() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+
+    @BeforeEach
+    void createChrome() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+
+    }
+}
